c067528ceb7bc01aa931877b9da2ebeedc611afb
Edmond Lam <elam3@stuy.edu>
2016-12-15 21:42:58 -0500

    Did the 1st and half of the second part of the homework

--- intro.js
+++ intro.js
@@ -1,8 +1,28 @@
 var printme = function(event) {
     console.log(this);
 };
-
+`
 for (i in document.getElementsByTagName('li')) {
     console.log("thing");
     i.addEventListener("mouseover", printme);
 }
+`
+var changeText = function(e) {
+    console.log(this);
+    var entry = document.getElementById('h');
+    entry.value=e
+};
+
+var item = document.getElementsByTagName('LI');
+item.addEventListener("mouseover", changeText);
+
+var addElement = function(e) {
+    console.log(this);
+    var list = document.getElementById('thelist');
+    var entry = document.createElement('li');
+    entry.appendChild(document.createTextNode("item "));
+    list.appendChild(entry);
+};
+
+var button = document.getElementById('pushit');
+button.addEventListener("click", addElement);
