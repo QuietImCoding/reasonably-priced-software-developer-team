a0dcda85b341ec8ef5a47e3a912ad0e21d335bdd
Daniel Monteagudo <dmonte3210@gmail.com>
2016-12-15 22:06:20 -0500

    finished hw

--- intro.js
+++ intro.js
@@ -1,28 +1,36 @@
-var printme = function(event) {
-    console.log(this);
-};
-`
-for (i in document.getElementsByTagName('li')) {
-    console.log("thing");
-    i.addEventListener("mouseover", printme);
-}
-`
-var changeText = function(e) {
-    console.log(this);
-    var entry = document.getElementById('h');
-    entry.value=e
-};
-
-var item = document.getElementsByTagName('LI');
-item.addEventListener("mouseover", changeText);
-
 var addElement = function(e) {
     console.log(this);
     var list = document.getElementById('thelist');
     var entry = document.createElement('li');
-    entry.appendChild(document.createTextNode("item "));
+    entry.appendChild(document.createTextNode("item " + document.getElementsByTagName("LI").length));
     list.appendChild(entry);
+    makeListeners();
 };
 
+var matchHeader = function(e) {    
+    var entry = document.getElementById('h');
+    entry.innerHTML=this.innerHTML;
+};
+
+var resetHeader = function(e) {
+    var header = document.getElementById('h');
+    header.innerHTML="Hello World!";
+};
+
+var removeElement = function(e) {
+    var list = document.getElementById("thelist");
+    list.removeChild(this);
+};
+
+var makeListeners = function(e) {
+    var nodeList = document.getElementsByTagName('LI');
+    for (i = 0; i < nodeList.length; i++) {
+	nodeList[i].addEventListener("mouseover", matchHeader);
+	nodeList[i].addEventListener("mouseout", resetHeader);
+	nodeList[i].addEventListener("click", removeElement);
+    }
+};
+
+makeListeners();
 var button = document.getElementById('pushit');
 button.addEventListener("click", addElement);
